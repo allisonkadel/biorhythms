e3da541af8fb51d9df8e82db7546d7bda6b7d283
Ally Kadel <akadel01@gmail.com>
2018-05-21 17:25:24 +0000

    add slug functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,13 @@ end
 post '/signup' do
   @user = User.create(params)
   session[:user_id] = @user.id
-
+  redirect '/dashboard/#{@user.slug}'
 end
 
 post '/login' do
   @user = User.find_by(:username => params[:username])
   session[:user_id] = @user.id
-
+  redirect '/dashboard/#{@user.slug}'
 end
 
 get '/dashboard/:slug' do
