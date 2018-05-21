1bf593b9ff3ed04c3b0bb02b5e22ff927c60029b
Ally Kadel <akadel01@gmail.com>
2018-05-21 20:44:34 +0000

    update show page

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,17 @@
+class EntriesController < ApplicationController
+
+  get '/entries/new' do
+    erb :'/entries/new'
+  end
+
+  post '/entries' do
+    current_user.entries.build(params).save
+    redirect '/entries'
+  end
+
+  get '/entries' do
+    @entries = current_user.entries
+    erb :'/entries/entries'
+  end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,13 @@ end
 post '/signup' do
   @user = User.create(params)
   session[:user_id] = @user.id
-
+  redirect '/entries'
 end
 
 post '/login' do
   @user = User.find_by(:username => params[:username])
   session[:user_id] = @user.id
-  binding.pry
+  redirect '/entries'
 end
 
 end
