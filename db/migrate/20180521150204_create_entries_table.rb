aec1f67b9f89a273d9d3820449525f428956118a
Ally Kadel <akadel01@gmail.com>
2018-05-21 20:46:02 +0000

    resolve merge

--- db/migrate/20180521150204_create_entries_table.rb
+++ db/migrate/20180521150204_create_entries_table.rb
@@ -1,7 +1,12 @@
 class CreateEntriesTable < ActiveRecord::Migration[5.2]
   def change
     create_table :entries do |t|
-      t.string :content
+      t.string :content_physical
+      t.integer :rating_physical
+      t.string :content_emotional
+      t.integer :rating_emotional
+      t.string :content_inrake dbtellectual
+      t.integer :rating_intellectual
       t.integer :user_id
       t.timestamps
     end
