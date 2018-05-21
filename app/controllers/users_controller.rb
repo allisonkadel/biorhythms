0eecdc5c4a93def83ed9454d0ed8cfd81316abf5
Ally Kadel <akadel01@gmail.com>
2018-05-21 17:37:44 +0000

    add user dashboard page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,12 +17,11 @@ end
 post '/login' do
   @user = User.find_by(:username => params[:username])
   session[:user_id] = @user.id
-  redirect '/dashboard/#{@user.slug}'
+  redirect "/dashboard/#{@user.slug}"
 end
 
 get '/dashboard/:slug' do
   @user = User.find_by_slug(params[:slug])
-  binding.pry
   erb :'/users/dashboard'
 end
 
