78482a0b97460eceda89ca4d94e82ffbf4c7f804
allisonkadel <akadel01@gmail.com>
2018-05-21 16:53:52 -0400

    Update entries_controller.rb

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,6 @@
 class EntriesController < ApplicationController
 
-<<<<<<< HEAD
+
   get '/entries/new' do
     erb :'/entries/new'
   end
@@ -14,7 +14,7 @@ class EntriesController < ApplicationController
     @entries = current_user.entries
     erb :'/entries/entries'
   end
-=======
+
 get '/entries/new' do
   erb :'/entries/new'
 end
@@ -23,6 +23,6 @@ post '/entries' do
   current_user.entries.build(params).save
   binding.pry
 end
->>>>>>> 169dd1a5a7c3d0365f3f0b20e6c47e5dd2f59b46
+
 
 end
