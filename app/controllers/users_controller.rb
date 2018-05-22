a3e217b9f43e984fe50939ac841750f8bad1be02
Ally Kadel <akadel01@gmail.com>
2018-05-22 02:14:43 +0000

    add logged_in conditional logic to post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,8 +32,12 @@ end
 
 post '/login' do
   @user = User.find_by(:username => params[:username])
-  session[:user_id] = @user.id
-  redirect "/dashboard/#{@user.slug}"
+  if @user && user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect "/dashboard/#{@user.slug}"
+  else
+    redirect '/login'
+  end
 end
 
 get '/dashboard/:slug' do
